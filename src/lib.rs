881c34b358d7ed81854daab01c164f53ce054ae6
sergorl <serg.neworl@gmail.com>
2018-02-09 17:01:09 +0300

    start work with server
    start work with server

---
+++ src/lib.rs
@@ -0,0 +1,96 @@
+extern crate futures;
+extern crate futures_cpupool;
+extern crate tokio;
+extern crate tokio_io;
+extern crate tokio_proto;
+extern crate tokio_service;
+extern crate bytes;
+
+use std::io;
+use std::str;
+use bytes::BytesMut;
+use tokio_io::codec::{Encoder, Decoder};
+use tokio_proto::pipeline::ServerProto;
+use tokio_service::Service;
+use futures::{future, Future};
+
+/// Request from client
+pub struct LineCodec;
+
+impl Decoder for LineCodec {
+    type Item = String;
+    type Error = io::Error;
+
+    fn decode(&mut self, buf: &mut BytesMut) -> io::Result<Option<String>> {
+        if let Some(i) = buf.iter().position(|&b| b == b'\n') {
+            // remove the serialized frame from the buffer.
+            let line = buf.split_to(i);
+
+            // Also remove the '\n'
+            buf.split_to(1);
+
+            // Turn this data into a UTF string and return it in a Frame.
+            match str::from_utf8(&line) {
+                Ok(s) => Ok(Some(s.to_string())),
+                Err(_) => Err(io::Error::new(io::ErrorKind::Other,
+                                             "invalid UTF-8")),
+            }
+        } else {
+            Ok(None)
+        }
+    }
+}
+
+impl Encoder for LineCodec {
+    type Item = String;
+    type Error = io::Error;
+
+    fn encode(&mut self, msg: String, buf: &mut BytesMut) -> io::Result<()> {
+        buf.extend(msg.as_bytes());
+        buf.extend(b"\n");
+        Ok(())
+    }
+}
+
+/// Protocol
+pub struct LineProto;
+
+use tokio_io::{AsyncRead, AsyncWrite};
+use tokio_io::codec::Framed;
+
+impl<T: AsyncRead + AsyncWrite + 'static> ServerProto<T> for LineProto {
+    // For this protocol style, `Request` matches the `Item` type of the codec's `Decoder`
+    type Request = String;
+
+    // For this protocol style, `Response` matches the `Item` type of the codec's `Encoder`
+    type Response = String;
+
+    // A bit of boilerplate to hook in the codec:
+    type Transport = Framed<T, LineCodec>;
+    type BindTransport = Result<Self::Transport, io::Error>;
+    fn bind_transport(&self, io: T) -> Self::BindTransport {
+        Ok(io.framed(LineCodec))
+    }
+}
+
+
+pub struct Echo;
+
+impl Service for Echo {
+    // These types must match the corresponding protocol types:
+    type Request = String;
+    type Response = String;
+
+    // For non-streaming protocols, service errors are always io::Error
+    type Error = io::Error;
+
+    // The future for computing the response; box it for simplicity.
+    type Future = Box<Future<Item = Self::Response, Error =  Self::Error>>;
+
+    // Produce a future for computing a response from a request.
+    fn call(&self, req: Self::Request) -> Self::Future {
+        // In this case, the response is immediate.
+        Box::new(future::ok(req))
+    }
+}
+
